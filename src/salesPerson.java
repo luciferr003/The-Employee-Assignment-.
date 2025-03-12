fec5a706b185c4634f627f4a1cf8673c2511535c
luciferr003 <luciferthemornigstar00@gmail.com>
2025-03-12 19:27:54 +0530

    Edited

--- src/salesPerson.java
+++ src/salesPerson.java
@@ -3,9 +3,16 @@ public class salesPerson extends employee
 {
 	int comission ;
 
-	public salesPerson(int comission ) 
+	public salesPerson(String name, String address, String phone_no, String gender, int age, int salary, int comission) 
 	{
+		super(name, address, phone_no, gender, age, salary);
 		this.comission = comission ;
+	}
+	
+	void display()
+	{
+		super.display();
+		System.out.println("Comission : "+ comission);
 	}
 
 }
